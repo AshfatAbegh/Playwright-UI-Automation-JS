8ee4d702bd6ae7c64f0ed6e767f8605aa477baae
Ashfat Al Rashid <mdabegh50@gmail.com>
2025-12-27 21:00:37 +0600

    Learning Playwright Automation JS

---
+++ tests/Softassertions.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test');
+
+test('Soft Assertions', async ({page}) => {
+ 
+    await page.goto('https://www.demoblaze.com/');
+
+    //hard assertions
+    /*await expect(page).toHaveTitle('STORE');
+    await expect(page).toHaveURL('https://www.demoblaze.com/');
+    await expect(page.locator('#nava')).toBeVisible();*/
+
+    //soft assertions
+    await expect.soft(page).toHaveTitle('STORE123');
+    await expect.soft(page).toHaveURL('https://www.demoblaze.com/');
+    await expect.soft(page.locator('#nava')).toBeVisible();
+});
