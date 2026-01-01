0611d20b94baeab865e5c6b379acd5f26ed54bce
Ashfat Al Rashid <mdabegh50@gmail.com>
2026-01-01 19:08:29 +0600

    Learning Playwright Automation JS

--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -8,7 +8,8 @@ test('Handle Dropdowns', async ({page}) => {
     //await page.locator("#country").selectOption({label: 'Canada'}); //select by label / visible text
    //await page.locator("#country").selectOption('Canada'); //select by visible text - direct value
   //await page.locator("#country").selectOption({value: 'canada'}); //select by value attribute
-    await page.locator("#country").selectOption({index: 1}); //select by index
+ //await page.locator("#country").selectOption({index: 1}); //select by index
+   await page.selectOption("#country", {label: 'Canada'}); // Alternative way to select option by label / visible text
     
 
     await page.waitForTimeout(5000);
